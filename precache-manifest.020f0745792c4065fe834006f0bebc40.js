1d2eae45085747a22e05c6bc92ecee8a12d88b39
ablossomhead <aimeecapps@hotmail.com>
2020-09-03 08:56:48 -0400

    Updates

---
+++ precache-manifest.020f0745792c4065fe834006f0bebc40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1009d1cf672c697e667492bc8ef33a31",
+    "url": "/ClassCalendar/index.html"
+  },
+  {
+    "revision": "f1ac84f45f6f6c193c31",
+    "url": "/ClassCalendar/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4d92e37b896d62bc76f8",
+    "url": "/ClassCalendar/static/css/main.332d379a.chunk.css"
+  },
+  {
+    "revision": "f1ac84f45f6f6c193c31",
+    "url": "/ClassCalendar/static/js/2.84c0a06a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ClassCalendar/static/js/2.84c0a06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d92e37b896d62bc76f8",
+    "url": "/ClassCalendar/static/js/main.71e74656.chunk.js"
+  },
+  {
+    "revision": "cd2425ef15f0b6c6ba06",
+    "url": "/ClassCalendar/static/js/runtime-main.8b0486e5.js"
+  }
+]);
